9feef689aeadde9f5f696949cfa4e7c66166d7e5
sergiocontrino <contrino.it@gmail.com>
2022-05-09 15:49:31 +0100

    a packege, hopefully

--- synthdatagen/main.py
+++ synthdatagen/main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from typing import NoReturn
 from get_args import get_args
-from from_db import get_tables
+from from_db import do_sampling
 from from_summaries import read_categorical_risks
 from from_summaries import read_continuous_risks
 
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     args = get_args()
     print("======", args.data_source.lower())
     if args.data_source.lower() == 'db':
-        get_tables(args)
+        do_sampling(args)
     elif args.numerical:
         read_continuous_risks(args)
     else:
