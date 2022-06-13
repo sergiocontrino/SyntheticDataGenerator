8c56e7f6a0d492cca16a5be89aafd70745c63941
sergiocontrino <contrino.it@gmail.com>
2022-06-13 16:44:19 +0100

    adds more tests to filter

--- test/test_filter.py
+++ test/test_filter.py
@@ -1,6 +1,12 @@
 """Test module for datasynth.filter"""
+import pytest
+
+import random
 import pandas as pd
-from datasynth.filter import filter_common_categories
+import numpy as np
+import pytest
+
+from datasynth.filter import filter_common_categories, values_per_column_all
 
 
 def test_value_count():
@@ -58,12 +64,68 @@ def test_filter_empty():
     initial_rows = df.shape[0]
     df = filter_common_categories(df, 2)
     outcome_rows = df.shape[0]
+    assert outcome_rows == 7
+    assert initial_rows > outcome_rows
+
+
+def test_filter_empty_warning():
+    df = pd.read_csv("/Users/sergio/git/SyntheticDataGenerator/test/contact.csv", index_col=False)
+    initial_rows = df.shape[0]
+    with pytest.warns(UserWarning):
+        df = filter_common_categories(df, 1)
+    outcome_rows = df.shape[0]
+    assert initial_rows == outcome_rows
+
+
+def test_filter_empty_big():
+    df = pd.read_csv("/Users/sergio/git/SyntheticDataGenerator/test/contact.csv", index_col=False)
+
+    initial_rows = df.shape[0]
+    df = filter_common_categories(df, 2)
+    outcome_rows = df.shape[0]
     print(df)
     assert outcome_rows == 7
     assert initial_rows > outcome_rows
 
 
-def test_filter_nodate_empty():
+def test_value_to_keep_simple():
+    data = {'col_1': [0, 0, 0, 1, 2, 2], 'col_2': [1, 1, 1, 2, 2, 2]}
+    df = pd.DataFrame(data=data)
+
+    expect = {'col_1': [0, 2], 'col_2': [1, 2]}
+    output = values_per_column_all(df, min_count=2)
+
+    assert expect == output
+
+
+def test_value_to_keep_nan():
+    data = {'col_1': [0, 0, 0, 1, 2, 2, np.nan], 'col_2': [1, 1, 1, 2, 2, 2, 2]}
+    df = pd.DataFrame(data=data)
+
+    expect = {'col_1': [0, 2], 'col_2': [1, 2]}
+    output = values_per_column_all(df, min_count=2)
+
+    assert expect == output
+
+
+def test_value_to_keep_nan():
+    data = {'col_1': [0, 0, 0, 1, 2, 2, np.nan, np.nan, np.nan], 'col_2': [1, 1, 1, 2, 2, 2, 3, 3, 3]}
+    df = pd.DataFrame(data=data)
+
+    expect = {'col_1': [0, 2, np.nan], 'col_2': [1, 2, 3]}
+    output = values_per_column_all(df, min_count=2)
+
+    assert set(expect['col_1']).difference(set(output['col_1'])) == {np.nan}
+
+
+def test_nan_multiindex():
+    df = pd.DataFrame(data={'col_1': [np.nan, 1, 2], 'col_2': [1, 2, 3], 'col_3': [4, 5, 6]})
+    df.set_index(['col_1', 'col_2'], inplace=True)
+    assert df.loc[(np.nan, 1), :].iloc[0] == 4
+    assert df.loc[([np.nan], [1]), :].values[0] == 4
+
+
+def test_filter_no_date_empty():
     df = pd.read_csv("/Users/sergio/git/SyntheticDataGenerator/test/filtrami_nodate.csv", index_col=False)
 
     initial_rows = df.shape[0]
