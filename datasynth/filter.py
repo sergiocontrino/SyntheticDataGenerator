c4e3333c3bae230cbaf0de232cb06f66a889fe05
sergiocontrino <contrino.it@gmail.com>
2022-06-13 16:43:15 +0100

    cleans filter (..) still issues

--- datasynth/filter.py
+++ datasynth/filter.py
@@ -3,9 +3,9 @@ Submodule for filtering a dataframe by individual frequencies of values per colu
 Provide guarantee that each column contains only 'frequent' values, i.e. only values that
 occur more often than a certain threshold min_count
 """
-from get_args import get_args
-import random
-import numpy as np
+import warnings
+from datasynth.get_args import get_args
+
 import pandas as pd
 
 
@@ -21,8 +21,7 @@ def values_per_column(df: pd.DataFrame, min_count: int) -> dict:
 def values_per_column_all(df: pd.DataFrame, min_count: int) -> dict:
     values_to_keep = {}
     for col in df.columns:
-        # print("-*", col)
-        df_counts = df.value_counts(col)
+        df_counts = df.value_counts(col, dropna=False)
         mask = df_counts >= min_count
         mask_index = df_counts[mask].index.to_list()
         values_to_keep[col] = mask_index
@@ -32,7 +31,6 @@ def values_per_column_all(df: pd.DataFrame, min_count: int) -> dict:
 def values_per_column_no_date(df: pd.DataFrame, min_count: int) -> dict:
     values_to_keep = {}
     for col in df.columns:
-        # print("-*", col)
         if "date" in col:
             values_to_keep[col] = df[col].tolist()
         else:
@@ -46,47 +44,30 @@ def values_per_column_no_date(df: pd.DataFrame, min_count: int) -> dict:
 def bool_dataframe_columns_to_string(dfin):
     df = dfin.convert_dtypes()
     bool_cols = df.dtypes[df.dtypes == pd.BooleanDtype()].index.tolist()
-    # print(bool_cols)
     if len(bool_cols) == 0:
         return df, []
     d = {True: 'TRUE', False: 'FALSE'}
 
     ddf = df.apply(lambda c: c.replace(d) if c.name in bool_cols else c)
-    # print(ddf)
     return ddf, bool_cols
 
 
-def filter_common_categories(df: pd.DataFrame, min_count: int = 3) -> pd.DataFrame:
-    df_filt = df.copy()
+def filter_common_categories(df: pd.DataFrame, min_count: int) -> pd.DataFrame:
+    if min_count <= 1:
+        warnings.warn('min_count <= 1 will cause no filter to be applied', UserWarning)
+        return df
 
-    # print("--init")
-    # print(df_filt)
+    df_filt = df.copy()
 
     # drop columns that only contain nan
     df_filt.dropna(how="all", axis=1, inplace=True)
     df_filt, bool_cols = bool_dataframe_columns_to_string(df_filt)
 
-    # print("--pre")
-    # print(df_filt)
-
-    nan_hash = random.getrandbits(16)
-    # df_filt.fillna(nan_hash, inplace=True, downcast='infer')
-    df_filt.fillna(np.nan, inplace=True, downcast='infer')
-    # print("--post")
-    # print(df_filt)
-
     values_to_keep = values_per_column(df_filt, min_count)
-    # print("d2", df_filt.shape)
-    # print(df_filt.columns)
 
     multi_index = tuple([values_to_keep[col] for col in df_filt.columns])
-    # print("d3", multi_index)
-    # print(df_filt.columns.to_list())
 
     df_mult = df_filt.reset_index().set_index(df_filt.columns.to_list())
-    # print("d4", df_mult.shape)
-
     df_mult = df_mult.loc[multi_index, :].reset_index().set_index('index')
 
-    # df_mult.replace(nan_hash, np.nan)
     return df_mult
